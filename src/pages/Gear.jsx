548a6a35d2607c9ac2435c8639a47343e8b27cff
brandon-banda <brandonvzx@gmail.com>
2022-10-11 08:08:07 -0500

    New stuff

--- src/pages/Gear.jsx
+++ src/pages/Gear.jsx
@@ -1,22 +1,59 @@
-import React from 'react';
-import Images from "../Images";
+import React from "react";
+import Images, { outdated } from "../Images";
 import "./Gear.scss";
 
 function Gear() {
-    return (
-      <div className='container'>
-        {Images.map(({ img, idx, title, desc, link, type }) => (
-          <div className='imgContainer'>
-            <h4> {title} </h4>
-            <img key={idx} src={img} alt='image' />
-            <p> {desc} </p>
-            <a href={link} class='button' target='_blank' rel='noreferrer'>
-              Check price
-            </a>
+  return (
+    <div className="container">
+      {Images.map(({ img, idx, title, desc, link, type }) => (
+        <div className="imgContainer">
+          <h4> {title} </h4>
+          <img key={idx} src={img} alt="imagee" />
+          <p> {desc} </p>
+          <a href={link} class="button" target="_blank" rel="noreferrer">
+            Check price
+          </a>
+        </div>
+      ))}
+      <div style={{ width: "100%" }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <div style={{ flex: 1, height: "1px", backgroundColor: "black" }} />
+
+          <div>
+            <p
+              style={{
+                width: "150px",
+                textAlign: "center",
+                fontSize: "26px",
+                color: "white",
+              }}
+            >
+              Outdated
+            </p>
           </div>
-        ))}
+
+          <div style={{ flex: 1, height: "1px", backgroundColor: "black" }} />
+        </div>
       </div>
-    );
-  }
-  
-  export default Gear;
+
+      {outdated.map(({ img, idx, title, desc, link, type }) => (
+        <div className="imgContainer">
+          <h4> {title} </h4>
+          <img key={idx} src={img} alt="imagee" />
+          <p> {desc} </p>
+          <a href={link} class="button" target="_blank" rel="noreferrer">
+            Check price
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Gear;
